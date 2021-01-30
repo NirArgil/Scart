369d6aa0fe627931821e2486a031c8cc25736e82
NIR <MAILMAIL10@GMAIL.COM>
2021-01-30 16:46:51 +0200

    update

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+import { Wrapper } from "./Header.styles";
+
+const Header = () => {
+    return (
+    <Wrapper>
+
+      <nav class="navbar sticky-top">
+        <a class="navbar-brand" href="">Scart</a>
+      </nav>
+
+    </Wrapper>    
+        
+ );
+};
+
+export default Header;
